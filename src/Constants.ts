348db79c6409dc6ea10005e5768aa97b9a5e0618
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2025-09-15 21:24:20 +0300

    fix: Update BASE_URL to use production URL and comment out local development URL

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,6 +1,6 @@
 // export const BASE_URL = "https://skills-ug-api.8technologies.net";
-// export const BASE_URL = "https://blit.blitxpress.com";
-export const BASE_URL = "http://localhost:8888/blitxpress";
+export const BASE_URL = "https://blit.blitxpress.com";
+// export const BASE_URL = "http://localhost:8888/blitxpress";
 export const API_URL = BASE_URL + "/api";
 export const TIMEOUT = 8000;
 export const MAX_RETRIES = 3;
